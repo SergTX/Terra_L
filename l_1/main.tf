b551f01e68291e8912b2e146712040f2ac22a5e3
SergTX <Sergey2623rus@gmail.com>
2023-12-14 20:19:56 -0600

    var file added

--- l_1/main.tf
+++ l_1/main.tf
@@ -18,6 +18,8 @@ terraform {
 }
 provider "aws" {
   region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 #resource "tls_private_key" "test_key" {
---
+++ l_1/variables.tf
@@ -0,0 +1,10 @@
+variable "access_key" {
+    default = "AKIA57G3H3NEZDQQFRUC"
+    type = string
+  
+}
+
+variable "secret_key" {
+  default = "K8MB+7fslC9n3y7v0lS9f5YnWz2C9zBcNA+4kb0+"
+  type = string
+}
